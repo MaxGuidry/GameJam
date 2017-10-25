d868ed667806f8f86daf01d0d75c20bba5a2ae08
Donray Williams <w.donray@yahoo.com>
2017-10-25 16:02:07 -0500

    Yes

---
+++ GameJam2017/Assets/Scripts/Scriptable Objects/StatSciptable.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName="StatSciptable", menuName = "Variables/StatSciptable", order = 1)]
+public class StatSciptable : ScriptableObject
+{
+    public List<FloatVariable> StatList;
+    public Dictionary<string, FloatVariable> StatDictionary;
+
+    void OnEnable()
+    {
+        StatDictionary = new Dictionary<string, FloatVariable>();
+        foreach (var Stat in StatList)
+        {
+            StatDictionary.Add(Stat.name, Stat);
+        }
+    }
+
+    public FloatVariable GetStat(string value)
+    {
+        return StatDictionary[value];
+    }
+
+    public bool _Alive;
+}
