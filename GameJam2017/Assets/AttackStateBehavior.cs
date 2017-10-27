7e1d6b00cedf85a398b2e58ccabbcd181f5ec7f7
MaxGuidry <mguidry7@outlook.com>
2017-10-27 17:40:09 -0500

    push for save

--- GameJam2017/Assets/AttackStateBehavior.cs
+++ GameJam2017/Assets/AttackStateBehavior.cs
@@ -6,7 +6,7 @@ public class AttackStateBehavior : StateMachineBehaviour {
 
 	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
 	override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	    animator.ResetTrigger("attack");
+	    //animator.ResetTrigger("attack");
 	}
 
 	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
@@ -16,7 +16,7 @@ public class AttackStateBehavior : StateMachineBehaviour {
 
 	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
 	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
-	    animator.ResetTrigger("attack");
+	    //animator.ResetTrigger("attack");
     }
 
 	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
--- GameJam2017/Assets/Scripts/MonoBehaviors/MonsterBehavior.cs
+++ GameJam2017/Assets/Scripts/MonoBehaviors/MonsterBehavior.cs
@@ -2,30 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.Assertions.Must;
 
-public class MonsterBehavior : MonoBehaviour
+public class MonsterBehavior : MonoBehaviour,IDamageable,IDamager
 {
     private NavMeshAgent agent;
     
    // public float attackDistance = .5f;
     public Animator m_anim;
 
-    public Transform target;
+    public StatSciptable stats;
+    private Transform target;
+
+    private float backupDamage;
     // Use this for initialization
     void Start()
     {
+        
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        stats.GetStat("EnemyHealth").Value = 100;
+        stats.GetStat("EnemyDamage").Value = 10;
+        backupDamage = stats.GetStat("EnemyDamage").Value;
         m_anim = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
-        agent.stoppingDistance = 5f;
+        agent.stoppingDistance = 6f;
+        agent.angularSpeed = 0;
+        StartCoroutine(TurnTo());
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!target || !target.gameObject)
+            return;
         var velocity = agent.velocity;
         m_anim.SetFloat("Speed", velocity.magnitude);
         agent.SetDestination(target.transform.position);
-        if (Vector3.Distance(target.transform.position, transform.position) < 5f)
+        if (Vector3.Distance(target.transform.position, transform.position) < 6f)
         {
             Attack();
         }
@@ -34,5 +47,45 @@ public class MonsterBehavior : MonoBehaviour
     void Attack()
     {
         m_anim.SetTrigger("Attack");
+        stats.GetStat("EnemyDamage").Value = backupDamage;
+    }
+
+    void NotAttacking()
+    {
+
+        stats.GetStat("EnemyDamage").Value = 0;
+    }
+    public void TakeDamage(float damage)
+    {
+        stats.GetStat("EnemyHealth").Value -= damage;
+    }
+
+    public void DoDamage(IDamageable defender)
+    {
+        defender.TakeDamage(stats.GetStat("EnemyDamage").Value);
+    }
+    public IEnumerator TurnTo()
+    {
+        while (true)
+        {
+            if (!target)
+                break;
+            if (!target.gameObject)
+                break;
+            Quaternion q = this.transform.rotation;
+            this.transform.LookAt(target.position + new Vector3(0,1,0));
+            this.transform.rotation = Quaternion.Slerp(q, this.transform.rotation, .1f);
+            yield return null;
+
+        }
+    }
+
+    public void StartRot()
+    {
+        StartCoroutine(TurnTo());
+    }
+    public void StopRot()
+    {
+        StopCoroutine("TurnTo");
     }
 }
