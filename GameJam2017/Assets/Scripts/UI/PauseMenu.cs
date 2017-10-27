bc17acbd2197bd670028fb7b4fcdaa876a0adeb3
MaxGuidry <mguidry7@outlook.com>
2017-10-27 10:32:10 -0500

    stuff

---
+++ GameJam2017/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject Pausemenu;
+
+    private bool paused = false;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(InputMap.KeyBinds["Pause"]))
+        {
+            paused = !paused;
+            Time.timeScale = (paused) ? 0 : 1;
+            Camera.main.gameObject.GetComponent<CameraController>().enabled =
+                !Camera.main.gameObject.GetComponent<CameraController>().enabled;
+            Pausemenu.SetActive(paused);
+        }
+    }
+}
